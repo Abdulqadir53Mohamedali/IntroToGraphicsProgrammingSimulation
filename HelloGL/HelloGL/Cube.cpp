a7101041e25a93105d27e96b750d23e4eca734c8
Abdulqadir Mohamedali <118472342+Abdulqadir53Mohamedali@users.noreply.github.com>
2025-03-27 16:48:14 +0000

    tutorial 8, file loading

--- HelloGL/HelloGL/Cube.cpp
+++ HelloGL/HelloGL/Cube.cpp
@@ -1,5 +1,7 @@
 #include "Cube.h"
 #include "GLUTCallbacks.h"
+#include <iostream>
+#include <fstream>
 
 
 Cube::Cube(float x, float y, float z) {
@@ -9,55 +11,91 @@ Cube::Cube(float x, float y, float z) {
     _position.z = z;
 
 }
-Vertex Cube::indexedVertices[] = { 1, 1, 1, -1, 1, 1, // v0,v1,
+//Vertex Cube::indexedVertices[] = { 1, 1, 1, -1, 1, 1, // v0,v1,
+//
+//-1,-1, 1, 1,-1, 1, // v2,v3
+//
+//1,-1,-1, 1, 1,-1, // v4,v5
+//
+//-1, 1,-1, -1,-1,-1 }; // v6,v7
+//Color Cube::indexedColors[] = { 1, 1, 1, 1, 1, 0, // v0,v1,
+//
+//1, 0, 0, 1, 0, 1, // v2,v3
+//
+//0, 0, 1, 0, 1, 1, // v4,v5
+//
+//0, 1, 0, 0, 0, 0 }; //v6,v7
+//GLushort Cube::indices[] = { 0, 1, 2, 2, 3, 0, // front
+//
+//0, 3, 4, 4, 5, 0, // right
+//
+//0, 5, 6, 6, 1, 0, // top
+//
+//1, 6, 7, 7, 2, 1, // left
+//
+//7, 4, 3, 3, 2, 7, // bottom
+//
+//4, 7, 6, 6, 5, 4 }; // back
 
--1,-1, 1, 1,-1, 1, // v2,v3
+Vertex* Cube::indexedVertices = nullptr;
+Color* Cube::indexedColors = nullptr;
+GLushort* Cube::indices = nullptr;
 
-1,-1,-1, 1, 1,-1, // v4,v5
-
--1, 1,-1, -1,-1,-1 }; // v6,v7
-Color Cube::indexedColors[] = { 1, 1, 1, 1, 1, 0, // v0,v1,
-
-1, 0, 0, 1, 0, 1, // v2,v3
-
-0, 0, 1, 0, 1, 1, // v4,v5
-
-0, 1, 0, 0, 0, 0 }; //v6,v7
-GLushort Cube::indices[] = { 0, 1, 2, 2, 3, 0, // front
-
-0, 3, 4, 4, 5, 0, // right
-
-0, 5, 6, 6, 1, 0, // top
-
-1, 6, 7, 7, 2, 1, // left
-
-7, 4, 3, 3, 2, 7, // bottom
-
-4, 7, 6, 6, 5, 4 }; // back
-
-
+int Cube::numVertices = 0;
+int Cube::numColors = 0;
+int Cube::numIndicies = 0;
 
 
 void Cube::Draw() 
 {
-    glPushMatrix();
-    glTranslatef(_position.x, _position.y, _position.z);
-    glRotatef(_rotation, 1.0f, 0.0f, 0.0f);
-    glEnableClientState(GL_VERTEX_ARRAY);
-    glEnableClientState(GL_COLOR_ARRAY);
-    glVertexPointer(3, GL_FLOAT, 0, indexedVertices);
-    glColorPointer(3, GL_FLOAT, 0, indexedColors);
+    if (indexedVertices != nullptr && indexedColors != nullptr && indices != nullptr) {
+        glPushMatrix();
+        glTranslatef(_position.x, _position.y, _position.z);
+        glRotatef(_rotation, 1.0f, 0.0f, 0.0f);
+        glEnableClientState(GL_VERTEX_ARRAY);
+        glEnableClientState(GL_COLOR_ARRAY);
+        glVertexPointer(3, GL_FLOAT, 0, indexedVertices);
+        glColorPointer(3, GL_FLOAT, 0, indexedColors);
 
-    glPushMatrix();
-    glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_SHORT, indices);
-    glPopMatrix();
+        glPushMatrix();
+        glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_SHORT, indices);
+        glPopMatrix();
 
-    glDisableClientState(GL_COLOR_ARRAY);
-    glDisableClientState(GL_VERTEX_ARRAY);
+        glDisableClientState(GL_COLOR_ARRAY);
+        glDisableClientState(GL_VERTEX_ARRAY);
 
-    glPopMatrix();
+        glPopMatrix();
+    }
+
 }
+bool Cube::Load(char* path) {
+    std::ifstream inFile;
+    inFile.open(path);
+    if (!inFile.good()) {
+        std::cerr << "Can't open text file" << path << std::endl;
+        return false;
+    }
+    inFile >> numVertices;
+    indexedVertices = new Vertex[numVertices];
+    for (int i = 0; i < numVertices; i++) {
+        inFile >> indexedVertices[i].x, indexedVertices[i].y, indexedVertices[i].z;
+    }  
+    inFile >> numColors;
+    indexedColors = new Color[numColors];
+    for (int i = 0; i < numColors; i++) {
+        inFile >> indexedColors[i].r, indexedColors[i].g, indexedColors[i].b;
 
+    }  
+    inFile >> numIndicies;
+    indices = new GLushort[numIndicies];
+    for (int i = 0; i < numIndicies; i++) {
+        inFile >> numIndicies[i];
+
+    }
+    inFile.close();
+
+    return true;
+}
 void Cube::Update() {
     _rotation += 1.0;
 
--- HelloGL/HelloGL/Cube.h
+++ HelloGL/HelloGL/Cube.h
@@ -9,18 +9,21 @@ class Cube
 {
 private :
 	Vector3 _position;
-	static  Vertex indexedVertices[];
-	static  Color indexedColors[];
-	static GLushort indices[];
+	static  Vertex* indexedVertices;
+	static  Color* indexedColors;
+	static GLushort* indices;
 	GLfloat _rotation;
 
 
 
 public :
+	static int numVertices, numColors, numIndicies;
+
 	Cube(float x,float y,float z);
 	~Cube();
 
 	void Draw();
+	static bool Load(char* path);
 	void Update();
 	void SetRotation();
 };
